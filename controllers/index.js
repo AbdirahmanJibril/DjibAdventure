aac672c8afafe08b6b24d2d90a07ce369e1412a7
ABDIRAHMAN JIBRIL MEAD <MY_NAME@example.com>
2021-11-19 15:46:36 +0000

    update home view

--- controllers/index.js
+++ controllers/index.js
@@ -1,36 +1,36 @@
-const express = require('express');
+const express = require('express')
 
-const { User, Trip, Post } = require('../models/user');
-const { cloudinary } = require('../cloudinary');
-const passport = require('passport');
-const https = require('https');
-const ejs = require('ejs');
-const util = require('util');
-const { deleteProfileImage } = require('../middleware');
-const { title } = require('process');
+const { User, Trip, Post } = require('../models/user')
+const { cloudinary } = require('../cloudinary')
+const passport = require('passport')
+const https = require('https')
+const ejs = require('ejs')
+const util = require('util')
+const { deleteProfileImage } = require('../middleware')
+const { title } = require('process')
 
-let date = new Date();
+let date = new Date()
 
-let dateOptions = { year: 'numeric', month: 'short', day: 'numeric' };
+let dateOptions = { year: 'numeric', month: 'short', day: 'numeric' }
 
-let bookingDate = date.toLocaleDateString('ar-EG', dateOptions);
+let bookingDate = date.toLocaleDateString('en-GB', dateOptions)
 
-let djibLocal = bookingDate;
-let fullYear = date.getFullYear();
+let djibLocal = bookingDate
+let fullYear = date.getFullYear()
 
 const homeRoute = (req, res) => {
   const url =
     'https://api.openweathermap.org/data/2.5/weather?q=Djibouti&appid=' +
     process.env.API_KEY +
-    '&units=metric';
+    '&units=metric'
   https.get(url, function (response) {
     response.on('data', function (data) {
-      const weatherData = JSON.parse(data);
-      const weatherDiscription = weatherData.weather[0].description;
-      const temp = weatherData.main.temp;
-      const feelsLike = weatherData.main.feels_like;
-      const icon = weatherData.weather[0].icon;
-      const urlimage = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+      const weatherData = JSON.parse(data)
+      const weatherDiscription = weatherData.weather[0].description
+      const temp = weatherData.main.temp
+      const feelsLike = weatherData.main.feels_like
+      const icon = weatherData.weather[0].icon
+      const urlimage = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
 
       res.render('home', {
         message: req.flash('loggedout'),
@@ -41,14 +41,14 @@ const homeRoute = (req, res) => {
         weatherIcon: urlimage,
         dateandtime: djibLocal,
         fullYear: fullYear,
-      });
-    });
-  });
-};
+      })
+    })
+  })
+}
 
 const getProfile = async (req, res) => {
   if (req.isAuthenticated()) {
-    let user = await User.findById({ _id: req.user._id });
+    let user = await User.findById({ _id: req.user._id })
 
     res.render('profile', {
       image: user.image,
@@ -56,181 +56,181 @@ const getProfile = async (req, res) => {
       fullYear,
       error: req.flash('error'),
       success: req.flash('success'),
-    });
+    })
   } else {
-    res.redirect('/login');
+    res.redirect('/login')
   }
-};
+}
 
 const updateProfile = async (req, res, next) => {
-  const { email, firstName, lastName, image } = req.body;
+  const { email, firstName, lastName, image } = req.body
 
   // any detail  update  if requested
-  const { user } = res.locals;
+  const { user } = res.locals
 
-  if (email) user.email = email;
-  if (firstName) user.firstName = firstName;
-  if (lastName) user.lastName = lastName;
+  if (email) user.email = email
+  if (firstName) user.firstName = firstName
+  if (lastName) user.lastName = lastName
   //image update if needed
   if (req.file) {
     if (user.image.filename)
-      await cloudinary.uploader.destroy(user.image.filename);
-    const { path, filename } = req.file;
-    user.image = { path, filename };
+      await cloudinary.uploader.destroy(user.image.filename)
+    const { path, filename } = req.file
+    user.image = { path, filename }
   }
-  await user.save();
-  const login = util.promisify(req.login.bind(req));
-  await login(user);
-  req.flash('success', 'You have update your profile');
+  await user.save()
+  const login = util.promisify(req.login.bind(req))
+  await login(user)
+  req.flash('success', 'You have update your profile')
 
   // req.session.success = 'Profile successfully updated!';
-  res.redirect('/profile');
-};
+  res.redirect('/profile')
+}
 
 //-----------------------------------Blogs---------------------------------------------------
 
 // Show a Post
 const showApost = async (req, res) => {
-  const id = req.params.id;
-  let Post = await User.find({ Post: { $elemMatch: { _id: id } } });
-  Post.forEach((foundPost) => {
+  const id = req.params.id
+  let Post = await User.find({ Post: { $elemMatch: { _id: id } } })
+  Post.forEach(foundPost => {
     res.render('showApost', {
       foundPost: foundPost.Post,
       image: foundPost.image,
       user: req.user,
       fullYear: fullYear,
-    });
-  });
-};
+    })
+  })
+}
 
 //show all blogs
 const getPosts = async (req, res) => {
   const myCustomLabels = {
     nextPage: 'next',
     prevPage: 'prev',
-  };
+  }
   const options = {
     page: req.query.page || 1,
     limit: 3,
     customLabels: myCustomLabels,
     sort: { 'Post._id': -1 },
-  };
+  }
   let posts = await User.aggregate([
     { $project: { 'image.path': 1, Post: 1 } },
     { $sort: { 'Post._id': -1 } },
-  ]);
+  ])
   User.aggregatePaginate(posts, options)
     .then(function (results) {
-      res.render('showBlogs', { showPosts: results, user: req.user, fullYear });
+      res.render('showBlogs', { showPosts: results, user: req.user, fullYear })
     })
     .catch(function (err) {
-      console.log(err);
-    });
-};
+      console.log(err)
+    })
+}
 
 //usere create blogpage
 const getRouteBlogPost = (req, res) => {
   if (req.isAuthenticated()) {
-    res.render('blogForm');
+    res.render('blogForm')
   } else {
-    res.redirect('/login');
+    res.redirect('/login')
   }
-};
+}
 
 //blog post submit route
 const postRoutBlogPost = async (req, res) => {
-  req.body.images = [];
+  req.body.images = []
   for (const file of req.files) {
     req.body.images.push({
       path: file.path,
       filename: file.filename,
-    });
+    })
   }
   const post = new Post({
     title: req.body.title,
     place: req.body.place,
     description: req.body.description,
     images: req.body.images,
-  });
-  post.save();
+  })
+  post.save()
 
   await User.findById(req.user._id, function (err, foundUser) {
     if (err) {
-      console.log(err);
+      console.log(err)
     } else {
-      foundUser.Post.push(post);
+      foundUser.Post.push(post)
       foundUser.save(function () {
         //  req.flash('message', 'You have successfully posted your blog');
 
-        res.redirect('/myblogs');
-      });
+        res.redirect('/myblogs')
+      })
     }
-  });
-};
+  })
+}
 //---------------------------Adventures & Booking Routes----------------------------
 
 const getMoucha = (req, res) => {
-  res.render('Moucha', { user: req.user, fullYear });
-};
+  res.render('Moucha', { user: req.user, fullYear })
+}
 // booking route
 const bookingRoute = (req, res) => {
   const trip = new Trip({
     Trip: req.body.destination,
     Date: req.body.adventureDate,
     person: req.body.poeple,
-  });
-  trip.save();
+  })
+  trip.save()
 
   User.findById(req.user._id, function (err, foundUser) {
     if (err) {
-      console.log(err);
+      console.log(err)
     } else {
-      foundUser.Adventure.push(trip);
+      foundUser.Adventure.push(trip)
       foundUser.save(function () {
         req.flash(
           'message',
           'You have successfully Booked',
           req.body.destination
-        );
-        res.redirect('/userspage');
-      });
+        )
+        res.redirect('/userspage')
+      })
     }
-  });
-};
+  })
+}
 
 // cancel booking route
 const cancelBookingRoute = async (req, res) => {
-  const id = req.body.toDelete;
-  const userId = req.user._id;
+  const id = req.body.toDelete
+  const userId = req.user._id
 
   await User.findOneAndUpdate(
     { _id: userId },
     { $pull: { Adventure: { _id: id } } },
     { new: true }
-  );
+  )
 
-  req.flash('message', 'You have Successfully Cancelled your Adventure');
-  res.redirect('/userspage');
-};
+  req.flash('message', 'You have Successfully Cancelled your Adventure')
+  res.redirect('/userspage')
+}
 
 //updte booking  Get-Route
 
 const bookingUpdate = async (req, res) => {
-  userId = req.user._id;
-  const id = req.params.id;
+  userId = req.user._id
+  const id = req.params.id
 
   let foundAdventure = await User.findById(
     { _id: userId },
     { Adventure: { $elemMatch: { _id: id } } }
-  );
+  )
 
   res.render('show', {
     AdventurePlace: foundAdventure.Adventure,
     foundImages: foundAdventure,
     fullYear: fullYear,
     user: req.user,
-  });
-};
+  })
+}
 
 // update booking POST-route
 const changeBooking = async (req, res) => {
@@ -244,18 +244,18 @@ const changeBooking = async (req, res) => {
       },
     },
     { new: true }
-  );
+  )
 
-  Booking.save();
+  Booking.save()
 
-  req.flash('message', 'You have successfully Updated', req.body.Trip);
-  res.redirect('/userspage');
-};
+  req.flash('message', 'You have successfully Updated', req.body.Trip)
+  res.redirect('/userspage')
+}
 
 //Delete images from  post
 
 const deleteMyimages = async (req, res) => {
-  console.log(req.body._id);
+  console.log(req.body._id)
   //   const userId=req.user._id;
 
   //   const user = await User.findById({_id:userId});
@@ -266,50 +266,50 @@ const deleteMyimages = async (req, res) => {
   //       });
   //   });
   // });
-};
+}
 
 //--------------------------------User Dash Board --------------------------------------
 
 //Blog Post-delete handler
 const postDelete = async (req, res) => {
-  const id = req.params.id;
-  const userId = req.user._id;
+  const id = req.params.id
+  const userId = req.user._id
 
-  const user = await User.findById({ _id: userId });
-  user.Post.forEach((foundImages) => {
-    foundImages.images.forEach(async (foundImage) => {
-      await cloudinary.uploader.destroy(foundImage.filename);
-    });
-  });
+  const user = await User.findById({ _id: userId })
+  user.Post.forEach(foundImages => {
+    foundImages.images.forEach(async foundImage => {
+      await cloudinary.uploader.destroy(foundImage.filename)
+    })
+  })
   await User.findOneAndUpdate(
     { _id: userId },
     { $pull: { Post: { _id: id } } },
     { new: true }
-  );
+  )
 
-  req.flash('message', 'You have Successfully deleted a Post');
-  res.redirect('/myblogs');
-};
+  req.flash('message', 'You have Successfully deleted a Post')
+  res.redirect('/myblogs')
+}
 
 //Post update/delete Get Route
 const updatePost = async (req, res) => {
-  const id = req.params.id;
+  const id = req.params.id
   let user = await User.findById(
     { _id: req.user._id },
     { Post: { $elemMatch: { _id: id } } },
     { multiple: true }
-  );
+  )
 
   res.render('update', {
     foundPost: user.Post,
     foundAdventure: user.Adventure,
     fullYear: fullYear,
     user: req.user,
-  });
-};
+  })
+}
 
 const editPost = async (req, res) => {
-  const id = req.params.id;
+  const id = req.params.id
 
   let user = await User.findOneAndUpdate(
     { _id: req.user._id, 'Post._id': id },
@@ -321,17 +321,17 @@ const editPost = async (req, res) => {
       },
     },
     { new: true }
-  );
+  )
 
-  user.save();
-  res.redirect('/myblogs');
-};
+  user.save()
+  res.redirect('/myblogs')
+}
 
 const getDashboard = (req, res) => {
   if (req.isAuthenticated()) {
     User.findById(req.user._id, function (err, foundUser) {
       if (err) {
-        console.log(err);
+        console.log(err)
       } else {
         res.render('dashboard', {
           user: foundUser.firstName,
@@ -340,31 +340,31 @@ const getDashboard = (req, res) => {
           foundPost: foundUser.Post,
           fullYear: fullYear,
           message: req.flash('message'),
-        });
+        })
       }
-    });
+    })
   } else {
-    res.redirect('/login');
+    res.redirect('/login')
   }
-};
+}
 const myBlogs = (req, res) => {
   if (req.isAuthenticated()) {
     User.findById(req.user._id, function (err, foundUser) {
       if (err) {
-        console.log(err);
+        console.log(err)
       } else {
         res.render('myblogs', {
           user: foundUser.firstName,
           foundPost: foundUser.Post,
           fullYear: fullYear,
           message: req.flash('message'),
-        });
+        })
       }
-    });
+    })
   } else {
-    res.redirect('/login');
+    res.redirect('/login')
   }
-};
+}
 
 //--------------------------------User Register / login /  passwor  reset routes---------
 
@@ -372,7 +372,7 @@ const usersRoute = (req, res) => {
   if (req.isAuthenticated()) {
     User.findById(req.user._id, function (err, foundUser) {
       if (err) {
-        console.log(err);
+        console.log(err)
       } else {
         res.render('userspage', {
           user: foundUser.firstName,
@@ -380,13 +380,13 @@ const usersRoute = (req, res) => {
           foundPost: foundUser.Post,
           fullYear: fullYear,
           message: req.flash('message'),
-        });
+        })
       }
-    });
+    })
   } else {
-    res.redirect('/login');
+    res.redirect('/login')
   }
-};
+}
 
 //user registeration route
 const registerRoute = (req, res) => {
@@ -394,21 +394,21 @@ const registerRoute = (req, res) => {
     user: req.user,
     message: req.flash('message'),
     error: req.flash('error'),
-  });
-};
+  })
+}
 
 const postRegisterRoute = async (req, res) => {
   if (req.file) {
-    const { path, filename } = req.file;
-    req.body.image = { path, filename };
-    console.log(req.file);
+    const { path, filename } = req.file
+    req.body.image = { path, filename }
+    console.log(req.file)
   }
-  let user = await User.findOne({ email: req.body.email });
+  let user = await User.findOne({ email: req.body.email })
 
   //check if email exist and delete profile image if email in use
   if (user) {
-    req.flash('error', ' account with that email address exist.');
-    return res.redirect('/Register');
+    req.flash('error', ' account with that email address exist.')
+    return res.redirect('/Register')
   }
 
   if (req.body.password === req.body.confirm) {
@@ -418,16 +418,16 @@ const postRegisterRoute = async (req, res) => {
       lastName: req.body.lastName,
       confirm: req.body.confirm,
       image: req.body.image,
-    });
+    })
 
-    const user = await User.register(newUser, req.body.password);
+    const user = await User.register(newUser, req.body.password)
     req.login(user, function (err) {
       if (err) {
-        deleteProfileImage(req);
-        req.flash('error', 'An Error occurred while registering');
-        res.redirect('/Register');
-      } else res.redirect('/dashboard');
-    });
+        deleteProfileImage(req)
+        req.flash('error', 'An Error occurred while registering')
+        res.redirect('/Register')
+      } else res.redirect('/dashboard')
+    })
 
     // req.authenticate("local")(req,res, () =>  {
     //   req.flash("message", "Welcome you have successfully registered");
@@ -441,7 +441,7 @@ const postRegisterRoute = async (req, res) => {
     //   deleteProfileImage(req);
     //   res.redirect("/Register");
   }
-};
+}
 module.exports = {
   homeRoute,
   getPosts,
@@ -464,4 +464,4 @@ module.exports = {
   getDashboard,
   showApost,
   getMoucha,
-};
+}
